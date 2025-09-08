d57ae17ddbb4cedec90be2d49a5abe639a3de656
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-09-08 19:28:20 +0530

    Check parent hash linkage

--- src/validation/chain_checks.h
+++ src/validation/chain_checks.h
@@ -6,3 +6,4 @@
 
 namespace gracecoin::validation {
 struct BlockHeaderView { std::string previous_hash; std::string merkle_root; std::uint64_t timestamp; std::uint32_t nonce; };
+bool has_parent_link(const BlockHeaderView& child, const std::string& parent_hash);
