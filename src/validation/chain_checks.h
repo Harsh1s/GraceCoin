806e9a21c635cca81f20c5bf26b258b85bb64bf7
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-09-10 22:40:01 +0530

    Validate monotonic block timestamps

--- src/validation/chain_checks.h
+++ src/validation/chain_checks.h
@@ -7,3 +7,4 @@
 namespace gracecoin::validation {
 struct BlockHeaderView { std::string previous_hash; std::string merkle_root; std::uint64_t timestamp; std::uint32_t nonce; };
 bool has_parent_link(const BlockHeaderView& child, const std::string& parent_hash);
+bool timestamp_is_monotonic(std::uint64_t previous_timestamp, std::uint64_t next_timestamp);
